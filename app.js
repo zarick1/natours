670ff21adf2de54c588bd151e43bf94a5b29ae73
zarick1 <krstozaric01@gmail.com>
2025-05-06 11:30:34 +0200

    first get req

--- app.js
+++ app.js
@@ -1,15 +1,21 @@
+const fs = require('fs');
 const express = require('express');
 
 const app = express();
 
-app.get('/', (req, res) => {
-  res
-    .status(200)
-    .json({ message: 'Hello from the server side!', app: 'Natours' });
-});
+const tours = JSON.parse(
+  fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
+);
 
-app.post('/', (req, res) => {
-  res.send('You can post ti this endpoint....');
+// Routing handler
+app.get('/api/v1/tours', (req, res) => {
+  res.status(200).json({
+    status: 'success',
+    results: tours.length,
+    data: {
+      tours: tours,
+    },
+  });
 });
 
 const port = 3000;
