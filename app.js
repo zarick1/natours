fe85de1b5afe0dda727d5b80895e193dd93ca4af
zarick1 <krstozaric01@gmail.com>
2025-05-06 19:54:03 +0200

    first patch req

--- app.js
+++ app.js
@@ -20,6 +20,21 @@ app.get('/api/v1/tours', (req, res) => {
   });
 });
 
+app.get('/api/v1/tours/:id', (req, res) => {
+  const id = +req.params.id;
+  const tour = tours.find((tour) => tour.id === id);
+
+  if (!tour)
+    return res.status(404).json({ status: 'Failed', message: 'Invalid ID' });
+
+  res.status(200).json({
+    status: 'success',
+    data: {
+      tour,
+    },
+  });
+});
+
 app.post('/api/v1/tours', (req, res) => {
   const newId = tours[tours.length - 1].id + 1;
   const newTour = Object.assign({ id: newId }, req.body);
@@ -33,6 +48,30 @@ app.post('/api/v1/tours', (req, res) => {
         status: 'success',
         data: {
           tour: newTour,
+        },
+      });
+    }
+  );
+});
+
+app.patch('/api/v1/tours/:id', (req, res) => {
+  const updateTour = tours.find((tour) => tour.id === +req.params.id);
+
+  if (!updateTour) return res.status(404).send('Not Found');
+
+  const propertiesToUpdate = Object.keys(req.body);
+  propertiesToUpdate.forEach((property) => {
+    updateTour[property] = req.body[property];
+  });
+  console.log(tours);
+  fs.writeFile(
+    `${__dirname}/dev-data/data/tours-simple.json`,
+    JSON.stringify(tours),
+    (err) => {
+      res.status(200).json({
+        status: 'success',
+        data: {
+          tour: updateTour,
         },
       });
     }
