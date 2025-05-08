f14b2603e6d1714234143569cedb221d5ab6629c
zarick1 <krstozaric01@gmail.com>
2025-05-08 16:51:40 +0200

    MVC

--- server.js
+++ server.js
@@ -1,9 +1,15 @@
-const app = require('./app');
+const mongoose = require('mongoose');
 const dotenv = require('dotenv');
+const app = require('./app');
 
 dotenv.config({ path: './config.env' });
 
-//console.log(process.env);
+const DB = process.env.DATABASE.replace(
+  '<PASSWORD>',
+  process.env.DATABASE_PASSWORD
+);
+
+mongoose.connect(DB).then(() => console.log('Date Base Successfuly connected'));
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
