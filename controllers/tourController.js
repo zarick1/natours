178893e56abf964aed7c7c0b88f6426189476e27
zarick1 <krstozaric01@gmail.com>
2025-05-08 17:27:06 +0200

    new MVC

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -1,15 +1,5 @@
 const Tour = require('./../models/tourModel');
 
-exports.checkBody = (req, res, next) => {
-  if (!(req.body.price && req.body.name))
-    return res.status(400).json({
-      status: 'faild',
-      message: 'Bad request, must contain price and name propery'
-    });
-  console.log(req.body);
-  next();
-};
-
 exports.getAllTours = (req, res) => {};
 
 exports.getTour = (req, res) => {};
